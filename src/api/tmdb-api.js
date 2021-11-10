4cc3bb0e90da76931e9d17e2b673383264488b17
Dai Jie Liu <20087611@mail.wit.ie>
2021-11-10 22:03:15 +0000

    Tagging upcoming movies as must-watch.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -88,3 +88,17 @@ export const getUpcomingMovies = (args) => {
         return json.results;
       });
   };
+
+  export const getUpcomingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDBKEY}&language=en-US&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+       throw error
+    });
+  };
